f0c2d2715167db7a90a280dc07c93e6b4b85c70a
Adithya Bhat G S <adithyabhatgs@gmail.com>
2025-11-13 18:42:40 +0530

    Modified the main.tf of iam

--- modules/iam/outputs.tf
+++ modules/iam/outputs.tf
@@ -1,6 +1,6 @@
 # Represents the ARN of Instance profile
 output "aws_ec2_instance_profile_name" {
-  value = local.ssm_instance_profile_arn
+  value = local.ssm_instance_profile_name
   # aws_iam_instance_profile.ec2_instance_profile.name
-  description = "The ARN of the Instance Profile containing the IAM role for the EC2 instance"
+  description = "The name of the Instance Profile containing the IAM role for the EC2 instance"
 }
