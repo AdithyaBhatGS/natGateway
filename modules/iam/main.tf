4adbf86d3c496e7713abd40155ec2d497400561b
Adithya Bhat G S <adithyabhatgs@gmail.com>
2025-11-13 18:13:59 +0530

    Modified the main.tf of iam module to consider the condition of using the exisisting role if not then create a new one

--- modules/iam/main.tf
+++ modules/iam/main.tf
@@ -1,3 +1,8 @@
+# Checking for existing IAM Role
+data "aws_iam_role" "existing" {
+  name = var.ec2_role_name
+}
+
 # Generating a Trust policy "AssumeRole" for EC2
 data "aws_iam_policy_document" "trust_policy_for_ec2" {
   statement {
@@ -10,17 +15,25 @@ data "aws_iam_policy_document" "trust_policy_for_ec2" {
   }
 }
 
+
 # Attaching the Trust policy for the IAM Role
 resource "aws_iam_role" "ec2_ssm_role" {
+  count              = try(data.aws_iam_role.existing.name, null) == null ? 1 : 0
   name               = var.ec2_role_name
   assume_role_policy = data.aws_iam_policy_document.trust_policy_for_ec2.json
   description        = var.ec2_role_description
   tags               = var.ec2_role_tags
 }
 
+# Outputting the IAM Role name and ARN
+locals {
+  ssm_role_name = try(data.aws_iam_role.existing.name, aws_iam_role.ssm_role[0].name)
+  ssm_role_arn  = try(data.aws_iam_role.existing.arn, aws_iam_role.ssm_role[0].arn)
+}
+
 # Attaching the managed policy for the IAM Role
 resource "aws_iam_role_policy_attachment" "ssm_managed_policy" {
-  role       = aws_iam_role.ec2_ssm_role.name
+  role       = local.ssm_role_name
   policy_arn = var.ssm_policy_arn
 }
 
